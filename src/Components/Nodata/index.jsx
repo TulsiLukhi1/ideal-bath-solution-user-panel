74b0525c53ec22358daedff01561be2e74a2bd8c
TulsiLukhi1 <105840817+TulsiLukhi1@users.noreply.github.com>
2024-07-09 00:39:53 +0530

    Create Nodata/ndex.jsx

---
+++ src/Components/Nodata/index.jsx
@@ -0,0 +1,14 @@
+import ImageNoData from "@/assets/images/no-data.svg";
+import Image from "next/image";
+
+const NoData = ({ className, width = 50, height }) => {
+  return (
+    <div
+      className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${className}`}
+    >
+      <Image src={ImageNoData} alt="loading" width={width} height={height} />
+    </div>
+  ); 
+};
+
+export default NoData;
